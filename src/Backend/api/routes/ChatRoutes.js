15482feefc87812bf9b3ef773e8d225b5e2feb9a
Seyi Obadeyi <obadeyi04@gmail.com>
2025-03-15 00:43:12 +0100

    fix:client app

--- src/Backend/api/routes/ChatRoutes.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-// eslint-disable-next-line no-undef
-const express = require("express");
-const router = express.Router();
-const { sendNewChat } = require("../../controllers/ChatController");
-
-router.post("/chat", sendNewChat);
--- src/Backend/models/Comment.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-const mongoose = require("mongoose")
-
-const CommentSchema = new mongoose.Schema({
-    text: { type: String, unique: true },
-})
-
-module.exports = mongoose.model("Comment" , CommentSchema);
